035e2bf00fc97d80c2dd0d3358ae2076affa1ed5
root <william.tavares@tvglobo.com.br>
2019-02-24 18:13:01 -0300

    First commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM alpine
+
+RUN apk add --no-cache\
+    python3\
+    bash &&\
+    pip3 install --upgrade pip &&\
+    pip3 install mkdocs
+
+RUN apk add -U tzdata
+RUN cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+
+RUn mkdir -p mkdocs
+WORKDIR mkdocs
+ARG MKDOCS_SITE
+ADD run.sh /run.sh
+RUN chmod +x /run.sh
+ENTRYPOINT ["/run.sh"] 
+CMD []
--- run.sh
+++ run.sh
@@ -1,15 +1,33 @@
-### MKDOCS Container
+#!/bin/bash
+
+
+ARG=$1
+
+
+if [[ $ARG == "produce" ]]; then
+
+    cd $MKDOCS_SITE
+    mkdocs build 1>&2
+    tar -czf site.tar.gz site/ 
+
+elif [[ $ARG == "serve" ]]; then
+
+    cd $MKDOCS_SITE
+    tar -xzf site.tar.gz 1>&2
+    mkdocs serve 1>&2
+
+else
+    cat <<USAGE
 This docker container runs mkdocs in two ways:
 
 1. Generate the site using the local path directory with the arg produce:
 
-```
 Example: docker run  --mount type=bind,source=\$(pwd)",target=/mkdocs -e MKDOCS_SITE=<MKDOCS_PROJECT> -it <IMAGE_NAME> produce
-```
 	
+
 2. Run the mkdocs with the tar.gz file produced before:
-
-```
 Example: docker run  -p8000:8000 --mount type=bind,source=\$(pwd)",target=/mkdocs -e MKDOCS_SITE=<MKDOCS_PROJECT> -it <IMAGE_NAME> serve
-```
 
+USAGE
+    exit
+fi
